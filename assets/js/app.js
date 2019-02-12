50975b14b74fae505e9e8b1dc59708d444384936
Floreshg <floreshg6@gmail.com>
2019-02-12 13:56:07 -0500

    promart

---
+++ assets/js/app.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+     $.ajax({
+        url: 'https://reqres.in/api/users?page=1',
+        contentType: "application/json",
+        dataType: 'json',
+        success: function(e){
+            const  userlist = $("#userlist");
+            $.each(e.data, function(index,element){
+                userlist.append(`<div class="col-sm">
+                <img src="${element.avatar}" alt="" class="rounded-circle mx-auto d-block">
+                <div>
+                <p class="text-center inline-block">${element.first_name} ${element.last_name}</p></div></div>`);
+            });
+        },
+
+        error: function(){
+            console.log("data not found");
+            
+        }
+    })
+      
+});
+ 
+ 
+ 
+ 
+
+$(document).ready(function(){
+    for(let i = 0; i < 4; i++) {
+        $('<div class="carousel-item"><a href="#"><img src="assets/img/banner.png" class="d-block w-100"><a></div>').appendTo('.carousel-inner');
+        $('<li data-target="#carousel" data-slide-to="'+i+'"></li>').appendTo('.carousel-indicators')
+    }
+
+    $('.carousel-item').first().addClass('active');
+    $('.carousel-indicators > li').first().addClass('active');
+    $('#carousel').carousel();
+});
